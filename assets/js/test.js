594883a629061721bfde32727fbffe78fcee08be
Endingratitan <leonwc200702@126.com>
2026-08-23 12:00:13 +0800

    test-pages-fixing and add js-test-page

---
+++ assets/js/test.js
@@ -0,0 +1,5 @@
+console.debug("This is a debug message from test.js");
+console.info("This is an info message from test.js");
+console.warn("This is a warning message from test.js");
+console.error("This is an error message from test.js");
+confirm("This is a confirmation dialog from test.js");
